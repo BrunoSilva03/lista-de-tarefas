ea4a77e3e6fea7d769ee36ab6cd37a82f7fd7068
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-01 15:12:30 -0300

    mais itens do formulário

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+
 function adicionar() {
-    
+    let tarefa = document.getElementById('idtarefa').value;
+    let data = document.getElementById('iddata').value;
+    let horario = document.getElementById('idhorario').value;
+
+    console.log("TAREFA: " + tarefa);
+    console.log("DATA: " + data);
+    console.log("HORÁRIO: " + horario);
 }
