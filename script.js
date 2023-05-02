c766399f5624f0481a9475875edc6a61e2681371
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-02 15:03:35 -0300

    que coisa chata esse erro

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 function adicionar() {
+    /*
     window.alert("Você clicou no botão");
+    
     var tarefa = window.document.getElementById('idtarefa').value;
     var data = window.document.getElementById('iddata').value;
     var horario = window.document.getElementById('idhorario').value;
     console.log("TAREFA: " + tarefa)
     console.log("DATA: " + data);
     console.log("HORÁRIO: " + horario);
+    
 
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('task-item');
+
+    const taskItemParagrafo = document.createElement('p');
+    taskItemParagrafo.innerHTML = 
+    `${tarefa}<br>
+    DIA: ${data}<br>
+    HORÁRIO: ${horario}`
+    
     var novaTarefa = document.createElement('div');
     novaTarefa.innerHTML = `<p>${tarefa}</p`
     novaTarefa.innerHTML += `<p>DIA: ${data}</p>`
     novaTarefa.innerHTML += `<p>HORÁRIO: ${horario}</p>`
-    window.location.href = "index.html"
+    */
+    
+    window.location.href = "menuzinho.html"
 }
 
-function retornar() {
-    window.location.href = "index.html"
-}
+
 
 
 function preencherformulario() {
