de18239500bef2e7f309d55cae373dffc326f173
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-02 11:29:00 -0300

    outras alterações no código!

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-
-
 function adicionar() {
-    let tarefa = document.getElementById('idtarefa').value;
-    let data = document.getElementById('iddata').value;
-    let horario = document.getElementById('idhorario').value;
-
-    console.log("TAREFA: " + tarefa);
+    window.alert("Você clicou no botão");
+    var tarefa = window.document.getElementById('idtarefa').value;
+    var data = window.document.getElementById('iddata').value;
+    var horario = window.document.getElementById('idhorario').value;
+    console.log("TAREFA: " + tarefa)
     console.log("DATA: " + data);
     console.log("HORÁRIO: " + horario);
-    
-    window.location.href = "index.html";
+
+    var novaTarefa = document.createElement('div');
+    novaTarefa.innerHTML = `<p>${tarefa}</p`
+    novaTarefa.innerHTML += `<p>DIA: ${data}</p>`
+    novaTarefa.innerHTML += `<p>HORÁRIO: ${horario}</p>`
+    window.location.href = "index.html"
 }
+
+function retornar() {
+    window.location.href = "index.html"
+}
+
+
+function preencherformulario() {
+    window.location.href = "formulario.html"
+}
+
