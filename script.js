2464d2dc573a5cb014ba01c92be38fe3d603f7d7
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-05-05 12:03:50 -0300

    Progresso antes do almoço

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
+function adicionarTask() {
+    desapareceApareceFormulario();
+    if(document.getElementById('formulario').style.display == 'block') {
+
+        confereFormulario();
+    }
+
+}
+
 
 function adicionar() {
-    
-    window.alert("Você clicou no botão");
-    
-    var tarefa = window.document.getElementById('idtarefa').value;
-    var data = window.document.getElementById('iddata').value;
-    var horario = window.document.getElementById('idhorario').value;
-    console.log("TAREFA: " + tarefa)
-    console.log("DATA: " + data);
-    console.log("HORÁRIO: " + horario);
+    var tarefa = document.getElementById('idtarefa').value;
+    var data = document.getElementById('iddata').value;
+    var horario = document.getElementById('idhorario').value;
 
-    var novoH1 = document.createElement("h1");
-    var textoAntigo = document.getElementById("textoParagrafo");
-    var novoTexto = document.createTextNode(textoAntigo.innerHTML);
-    novoH1.appendChild(novoTexto);
-    textoAntigo.replaceChild(novoH1, textoAntigo);
-    
-/*
-    let textinho = document.getElement('p#texto');
-    textinho.document.setElement('Novo texto aí galera pessoal!');
-    
-    const taskItem = document.createElement('div');
-    taskItem.classList.add('task-item');
+    desapareceApareceFormulario();
 
-    const taskItemParagrafo = document.createElement('p');
-    taskItemParagrafo.innerHTML = 
-    `${tarefa}<br>
-    DIA: ${data}<br>
-    HORÁRIO: ${horario}`
-    
     /*
-    var novaTarefa = document.createElement('div');
-    novaTarefa.innerHTML = `<p>${tarefa}</p`
-    novaTarefa.innerHTML += `<p>DIA: ${data}</p>`
-    novaTarefa.innerHTML += `<p>HORÁRIO: ${horario}</p>`
+    var tasks = document.getElementById('div.tasks');
     
+    var novaTask = document.createElement('div');
     
-    tarefa = '';
-    data = '';
-    horario = '';
+    novaTask = `<p>${tarefa}</p>
+               DATA: ${data}
+               HORÁRIO: ${horario}`;
+               
+    tasks.appendChild(novaTask);
+    
     */
-    
-    window.location.href = "menuzinho.html"
 }
 
 
-function funcione() {
-    window.alert("Funcione imediatamente!")
+function desapareceApareceFormulario() {
+    var displayForm = document.getElementById('formulario').style.display;
+
+    if (displayForm == 'none') {
+        document.getElementById('formulario').style.display = 'block';
+    } else {
+        document.getElementById('formulario').style.display = 'none';
+    }
 }
 
+function confereFormulario() {
+    var tarefa = document.getElementById('idtarefa').value;
+    var data = document.getElementById('iddata').value;
+    var horario = document.getElementById('idhorario').value;
 
+    if(tarefa == '' || data == '' || horario == '') {
+        alert('Preencha todos os campos por favor.')
+    }
+}
 
 function preencherformulario() {
     window.location.href = "formulario.html"
